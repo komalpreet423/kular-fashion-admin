5ba51693dc9b872c8ac6b4bfcc9027c63f48943c
Satpal Singh Sekhon <satpalinfilogix@gmail.com>
2025-03-05 21:02:36 +0530

    Changed paid_amount column

--- database/migrations/2025_02_28_052212_create_layaways_table.php
+++ database/migrations/2025_02_28_052212_create_layaways_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->unsignedBigInteger('customer_id')->index();
             $table->unsignedBigInteger('order_id')->nullable()->index();
             $table->decimal('total_amount', 10, 2)->nullable(); 
-            $table->decimal('amount_paid', 10, 2)->default(0);
+            $table->decimal('paid_amount', 10, 2)->default(0);
             $table->decimal('balance', 10, 2)->nullable();
             $table->tinyInteger('status')->default(0);
             $table->unsignedBigInteger('sales_person_id')->nullable();
