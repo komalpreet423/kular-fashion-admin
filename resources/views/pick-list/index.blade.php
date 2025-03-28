574f463e88a7de3d34c0cda786ce59e2574afcdf
AmanAharwal <amaninfilogix@gmail.com>
2025-03-28 11:10:38 +0530

    Fix datatable responsive and filter issue

--- resources/views/pick-list/index.blade.php
+++ resources/views/pick-list/index.blade.php
@@ -23,7 +23,7 @@
                                     <label for="branch">Branch</label>
                                     <select id="branch" name="branch" class="form-control">
                                         @foreach ($branches as $branch)
-                                        <option value="{{ $branch->id }}" @selected(request('branch', auth()->user()->branch_id) == $branch->id)>
+                                        <option value="{{ $branch->id }}" @selected(old('branch', $defaultBranch)==$branch->id)>
                                             {{ $branch->name }}
                                         </option>
                                         @endforeach
@@ -32,7 +32,10 @@
 
                                 <div class="col-md-4">
                                     <label for="date">Select Date</label>
-                                    <input type="text" id="date" name="date" class="form-control" autocomplete="off" value="{{ request('date') ?? now()->format('Y-m-d') }}">
+                                    @php
+                                    $defaultDate = request('date') ?? now()->subDay()->format('Y-m-d');
+                                    @endphp
+                                    <input type="text" id="date" name="date" class="form-control" autocomplete="off" value="{{ $defaultDate }}">
                                 </div>
 
                                 <div class="col-md-4 mt-4">
@@ -46,40 +49,42 @@
 
                 <div class="card">
                     <div class="card-body">
-                        <table id="datatable-buttons" class="table table-bordered table-striped dt-responsive nowrap w-100">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Branch</th>
-                                    <th>Department</th>
-                                    <th>Make</th>
-                                    <th>Product Type</th>
-                                    <th>Price</th>
-                                    <th>Sold Price</th>
-                                    <th>Description</th>
-                                    <th>Color</th>
-                                    <th>Size</th>
-                                    <th>Created At</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($orderItems as $key => $item)
-                                <tr>
-                                    <td>{{ ++$key }}</td>
-                                    <td>{{ $item->branch->name ?? 'N/A' }}</td>
-                                    <td>{{ $item->product->department->name ?? 'N/A' }}</td>
-                                    <td>{{ $item->brand_name ?? 'N/A' }}</td>
-                                    <td>{{ $item->product->productType->name ?? 'N/A' }}</td>
-                                    <td>{{ $item->original_price }}</td>
-                                    <td>{{ $item->changed_price }}</td>
-                                    <td>{{ $item->description ?? 'N/A' }}</td>
-                                    <td>{{ $item->color_name ?? 'N/A' }}</td>
-                                    <td>{{ $item->size ?? 'N/A' }}</td>
-                                    <td>{{ $item->created_at->format('Y-m-d') }}</td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                        <div class="table-responsive">
+                            <table id="datatable-buttons" class="table table-bordered table-striped dt-responsive nowrap w-100">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Branch</th>
+                                        <th>Department</th>
+                                        <th>Make</th>
+                                        <th>Product Type</th>
+                                        <th>Price</th>
+                                        <th>Sold Price</th>
+                                        <th>Description</th>
+                                        <th>Color</th>
+                                        <th>Size</th>
+                                        <th>Created At</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($orderItems as $key => $item)
+                                    <tr>
+                                        <td>{{ ++$key }}</td>
+                                        <td>{{ $item->branch->name ?? 'N/A' }}</td>
+                                        <td>{{ $item->product->department->name ?? 'N/A' }}</td>
+                                        <td>{{ $item->brand_name ?? 'N/A' }}</td>
+                                        <td>{{ $item->product->productType->name ?? 'N/A' }}</td>
+                                        <td>{{ $item->original_price }}</td>
+                                        <td>{{ $item->changed_price }}</td>
+                                        <td>{{ $item->description ?? 'N/A' }}</td>
+                                        <td>{{ $item->color_name ?? 'N/A' }}</td>
+                                        <td>{{ $item->size ?? 'N/A' }}</td>
+                                        <td>{{ $item->created_at->format('Y-m-d') }}</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
 
@@ -100,21 +105,19 @@
         $("#datatable-buttons").DataTable({
             lengthChange: false
             , buttons: ["copy", "excel", "pdf", "colvis"]
-            , order: [], 
-            columnDefs: [{
-                    targets: "_all"
-                    , orderable: true
-                } 
-            ]
+            , order: []
+            , columnDefs: [{
+                targets: "_all"
+                , orderable: true
+            }]
         }).buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)");
 
         $(".dataTables_length select").addClass("form-select form-select-sm");
 
-        var selectedDate = "{{ request('date') }}" || new Date().toISOString().slice(0, 16).replace("T", " ");
-
+        var selectedDate = "{{ $defaultDate }}";
         flatpickr("#date", {
-            enableTime: true
-            , dateFormat: "Y-m-d "
+            enableTime: false
+            , dateFormat: "Y-m-d"
             , defaultDate: selectedDate
             , allowInput: true
         });
