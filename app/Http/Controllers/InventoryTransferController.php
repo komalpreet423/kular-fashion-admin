875431b5a2ee2f48fb765f28996ada408261f32c
VishalInfilogix <vishalInfilogix@gmail.com>
2025-04-23 15:28:53 +0530

    product list orders and inventry transfer issue solved

--- app/Http/Controllers/InventoryTransferController.php
+++ app/Http/Controllers/InventoryTransferController.php
@@ -91,13 +91,14 @@ class InventoryTransferController extends Controller
             $quantity = $value['quantity'];
 
             $productQuantity = ProductQuantity::where('id', $productQuantityId)->with('sizes.sizeDetail', 'colors.colorDetail')->first();
-
+            
+            
             InventoryItem::create([
                 'inventroy_transfer_id' => $inventoryTransfer->id,
                 'product_id'            => $productQuantity->product_id,
                 'product_quantity_id'   => $productQuantityId,
-                'product_color_id'      => $productQuantity->colors->colorDetail->id,
-                'product_size_id'       => $productQuantity->sizes->sizeDetail->id,
+                'product_color_id'      => $productQuantity->product_color_id,
+                'product_size_id'       => $productQuantity->product_size_id,
                 'brand_id'              => $value['brand_id'],
                 'quantity'              => $quantity,
             ]);
