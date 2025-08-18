f62568702c80a26c18e23135673c5af23dffa070
VishalInfilogix <vishalInfilogix@gmail.com>
2025-08-18 15:07:03 +0530

    add Filters in category api

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,26 +4,37 @@ use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
     public function Index()
     {
         $categories = Category::whereNull('parent_id')
-            ->with(['childrenRecursive' => function($query) {
+            ->with(['childrenRecursive' => function ($query) {
                 $query->select([
-                    'id', 'slug', 'name', 'parent_id', 'image', 
-                    'summary', 'description', 'heading',
-                    'meta_title', 'meta_keywords', 'meta_description', 'status'
+                    'id',
+                    'slug',
+                    'name',
+                    'parent_id',
+                    'image',
+                    'summary',
+                    'description',
+
                 ]);
             }])
             ->select([
-                'id', 'slug', 'name', 'parent_id', 'image', 
-                'summary', 'description', 'heading',
-                'meta_title', 'meta_keywords', 'meta_description', 'status'
+                'id',
+                'slug',
+                'name',
+                'parent_id',
+                'image',
+                'summary',
+                'description',
+
             ])
             ->get();
-        
+
 
         return response()->json([
             'success' => true,
@@ -42,22 +53,100 @@ class CategoryController extends Controller
                 'message' => 'Category not found.'
             ], 404);
         }
-
+        $products = $category->categoriesProduct->pluck('products')->filter();
+        $filters = $this->buildProductFilters($products);
         return response()->json([
             'success' => true,
             'data' => $category,
+            'filters' => $filters,
             'message' => 'Category retrieved successfully.'
         ]);
     }
 
+
+    protected function buildProductFilters($products, $visibleFilters = [], $collection = null)
+    {
+        $allFilters = [
+            'price' => ['min' => PHP_FLOAT_MAX, 'max' => 0],
+            'brands' => [],
+            'sizes' => [],
+            'colors' => [],
+            'product_types' => [],
+            'tags' => []
+        ];
+
+        foreach ($products as $product) {
+            $price = $product->sale_price ?? $product->price;
+            $allFilters['price']['min'] = min($allFilters['price']['min'], $price);
+            $allFilters['price']['max'] = max($allFilters['price']['max'], $price);
+
+            if ($product->brand) {
+                $allFilters['brands'][$product->brand->id] = [
+                    'id' => $product->brand->id,
+                    'name' => $product->brand->name,
+                    'slug' => $product->brand->slug ?? Str::slug($product->brand->name)
+                ];
+            }
+
+            if ($product->productType) {
+                $allFilters['product_types'][$product->productType->id] = [
+                    'id' => $product->productType->id,
+                    'name' => $product->productType->name,
+                    'slug' => $product->productType->slug ?? Str::slug($product->productType->name)
+                ];
+            }
+
+            foreach ($product->sizes ?? [] as $size) {
+                if ($size->sizeDetail) {
+                    $sizeName = $size->sizeDetail->size ?? $size->sizeDetail->new_code ?? $size->sizeDetail->old_code ?? "Size {$size->sizeDetail->id}";
+                    $allFilters['sizes'][$size->sizeDetail->id] = [
+                        'id' => $size->sizeDetail->id,
+                        'size' => $sizeName,
+                        'name' => $sizeName
+                    ];
+                }
+            }
+
+            foreach ($product->colors ?? [] as $color) {
+                if ($color->colorDetail) {
+                    $allFilters['colors'][$color->colorDetail->id] = [
+                        'id' => $color->colorDetail->id,
+                        'name' => $color->colorDetail->name,
+                        'hex' => $color->colorDetail->ui_color_code ?? $color->colorDetail->hex ?? '#000000'
+                    ];
+                }
+            }
+
+            foreach ($product->tags ?? [] as $tag) {
+                $allFilters['tags'][$tag->id] = [
+                    'id' => $tag->id,
+                    'name' => $tag->name,
+                    'slug' => $tag->slug
+                ];
+            }
+        }
+
+        $allFilters['price'] = [
+            'min' => $allFilters['price']['min'] === PHP_FLOAT_MAX ? 0 : (float)number_format($allFilters['price']['min'], 2, '.', ''),
+            'max' => $allFilters['price']['min'] === $allFilters['price']['max']
+                ? $allFilters['price']['min'] + 100
+                : (float)number_format($allFilters['price']['max'], 2, '.', '')
+        ];
+
+        if ($collection?->listingOption?->hide_filters) {
+            return [];
+        }
+
+        return $allFilters;
+    }
     public function GetCategoryProducts($slug, Request $request)
     {
         $category = Category::where('slug', $slug)->firstOrFail();
         $categoryIds = $category->childrenRecursive->pluck('id')->push($category->id);
 
-        $products = Product::whereHas('categories', function($query) use ($categoryIds) {
-                $query->whereIn('id', $categoryIds);
-            })
+        $products = Product::whereHas('categories', function ($query) use ($categoryIds) {
+            $query->whereIn('id', $categoryIds);
+        })
             ->with([
                 'brand',
                 'department',
